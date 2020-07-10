7d6d99c3e3a3107512e46740d057939d11638ef8
Marcelina Golinska <marcagol@gmail.com>
2020-07-10 21:15:41 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+console.log("Tym razem umiem więcej 😃");
+let titleChangeButton = document.querySelector(".mainBody__titleButton");
+let title = document.querySelector(".title");
+let newTitle = document.querySelector(".newTitle");
+titleChangeButton.addEventListener("click", function () {
+  title.classList.toggle("newTitle");
+
+  if (title.classList.contains("newTitle")) {
+    title.innerText = "Fotograf Marcelina Golińska";
+    titleChangeButton.innerText = "Zmień tytuł na stary";
+  } else {
+    title.innerText = "Marcelina Golińska - fotograf";
+    titleChangeButton.innerText = "Zmień tytuł strony";
+  }
+});
+console.log("Tytuł strony został zmieniony");
