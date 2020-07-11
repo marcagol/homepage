395ef82f912a7c03ab400727749aa22fbbf749c6
Marcelina Golinska <marcagol@gmail.com>
2020-07-11 15:59:26 +0200

    restructured functions

--- js/script.js
+++ js/script.js
@@ -1,20 +1,36 @@
-function welcome () {
-  console.log("Tym razem umiem więcej 😃");
+{
+  const welcome = () => {
+    console.log("Tym razem umiem więcej 😃");
+  };
+
+  const toggleTitle = () => {
+    const title = document.querySelector(".title");
+    const titleChangeButton = document.querySelector(".titleChangeButton");
+
+
+    title.classList.toggle("newTitle");
+
+
+    if (title.classList.contains("newTitle")) {
+      title.innerText = "Fotograf Marcelina Golińska";
+      titleChangeButton.innerText = "Zmień tytuł na stary";
+
+    }
+    else {
+      title.innerText = "Marcelina Golińska - fotograf";
+      titleChangeButton.innerText = "Zmień tytuł strony";
+
+    }
+  };
+  const init = () => {
+    const titleChangeButton = document.querySelector(".main__titleButton");
+    titleChangeButton.addEventListener("click", toggleTitle);
+
+
+    welcome();
+
+  };
+
+  init();
+
 }
-
-welcome();
-let titleChangeButton = document.querySelector(".main__titleButton");
-let title = document.querySelector(".title");
-let newTitle = document.querySelector(".newTitle");
-titleChangeButton.addEventListener("click", function () {
-  title.classList.toggle("newTitle");
-
-  if (title.classList.contains("newTitle")) {
-    title.innerText = "Fotograf Marcelina Golińska";
-    titleChangeButton.innerText = "Zmień tytuł na stary";
-  } else {
-    title.innerText = "Marcelina Golińska - fotograf";
-    titleChangeButton.innerText = "Zmień tytuł strony";
-  }
-});
-console.log("Tytuł strony został zmieniony");
